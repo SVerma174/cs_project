0c46557d718c61ab4bff5478067ca0eba9fa9cc3
Jv2350 <tp8375735@gmail.com>
2024-07-14 21:08:40 +0530

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import Navbar from "./componets/Navbar"
-import Header from "./componets/Navbar/Header"
-import SearchBar from "./componets/Navbar/Header/Searchbar"
-import JobCard from "./componets/Navbar/Header/Searchbar/Jobcard"
-import jobData from "./JobDummyData"
+import Navbar from "./componets/Navbar";
+import Header from "./componets/Header";
+import SearchBar from "./componets/Searchbar";
+import JobCard from "./componets/Jobcard";
+import jobData from "./JobDummyData";
 function App() {
- 
-
   return (
-  <div>
-    <Navbar />
-    <Header />
-    <SearchBar />
-    {jobData.map((job) => (
-      <JobCard key={job.id} job={job}/>
-    ))}
-
-  </div>
-  )
+    <div>
+      <Navbar />
+      <Header />
+      <SearchBar />
+      {jobData.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
